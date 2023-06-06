908d517b78773f0251cbbb854fb7a6beef003924
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2023-06-06 01:10:05 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
