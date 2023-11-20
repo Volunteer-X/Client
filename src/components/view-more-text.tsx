be8328835f76cca2c27591018cc4b2a65587cd1f
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2023-11-20 13:17:33 +0000

    Refactor ViewMoreText component to use hooks

--- src/components/view-more-text.tsx
+++ src/components/view-more-text.tsx
@@ -1,130 +1,49 @@
 import {
   LayoutChangeEvent,
+  NativeSyntheticEvent,
   StyleProp,
   StyleSheet,
+  TextLayoutEventData,
   TextStyle,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Text } from 'react-native-paper';
 
 type Prop = {
   children: React.ReactNode;
-  textStyles?: StyleProp<TextStyle>;
+  // textStyles?: StyleProp<TextStyle>;
   numberOfLines: number;
-  afterExpand?: () => void;
-  afterCollapse?: () => void;
-  onTextLayout?: (event: any) => void;
-  renderViewMore?: (onPress: () => void) => React.ReactNode;
-  readerViewLess?: (onPress: () => void) => React.ReactNode;
 };
 
 export const ViewMoreText = ({
   children,
-  textStyles,
+  // textStyles,
   numberOfLines = 3,
-  afterExpand = () => {},
-  afterCollapse = () => {},
-  onTextLayout,
-  renderViewMore,
-  readerViewLess,
 }: Prop) => {
-  let trimmedTextHeight: number | null = null;
-  let fullTextHeight: number | null = null;
-  let shouldShowMore = false;
+  const [textshown, setTextShown] = React.useState(false);
+  const [lengthMore, setLengthMore] = React.useState(false);
 
-  const [isFullTextShown, setIsFullTextShown] = React.useState(true);
-  const [_numberOfLines, setNumberOfLines] = React.useState<number | undefined>(
-    numberOfLines,
-  );
-
-  const hideFullText = () => {
-    if (isFullTextShown && trimmedTextHeight && fullTextHeight) {
-      shouldShowMore = trimmedTextHeight < fullTextHeight;
-      setIsFullTextShown(false);
-    }
+  const toggleNumberOfLines = () => {
+    setTextShown(!textshown);
   };
 
-  const onLayoutTrimmedText = (event: LayoutChangeEvent) => {
-    const { height } = event.nativeEvent.layout;
-    trimmedTextHeight = height;
-    console.log('trimmedTextHeight', trimmedTextHeight);
-
-    hideFullText();
-  };
-
-  const onLayoutFullText = (event: LayoutChangeEvent) => {
-    const { height } = event.nativeEvent.layout;
-    fullTextHeight = height;
-    console.log('fullTextHeight', fullTextHeight);
-
-    hideFullText();
-  };
-
-  const onPressMore = () => {
-    setNumberOfLines(undefined);
-    afterExpand();
-  };
-
-  const onPressLess = () => {
-    setNumberOfLines(numberOfLines);
-    afterCollapse();
-  };
-
-  const getWrapperStyle = () => {
-    if (isFullTextShown) {
-      return {
-        opacity: 0,
-      };
-    }
-    return {};
-  };
-
-  const _renderViewMore = () => (
-    <Text onPress={onPressMore} style={styles.viewMoreText}>
-      View more
-    </Text>
-  );
-
-  const _renderViewLess = () => (
-    <Text onPress={onPressLess} style={styles.viewMoreText}>
-      View less
-    </Text>
-  );
-
-  const renderFooter = () => {
-    if (shouldShowMore) {
-      if (numberOfLines > 0) {
-        return (renderViewMore || _renderViewMore)(onPressMore);
-      }
-      return (readerViewLess || _renderViewLess)(onPressLess);
-    }
-    return null;
-  };
-
-  const renderFullText = () => {
-    if (isFullTextShown) {
-      return (
-        <View onLayout={onLayoutFullText} style={styles.fullTextWrapper}>
-          <Text style={textStyles}>{children}</Text>
-        </View>
-      );
-    }
-    return null;
+  const onTextLayout = (e: NativeSyntheticEvent<TextLayoutEventData>) => {
+    setLengthMore(e.nativeEvent.lines.length >= numberOfLines);
   };
 
   return (
-    <View style={getWrapperStyle()}>
-      <View onLayout={onLayoutTrimmedText}>
-        <Text
-          onTextLayout={onTextLayout}
-          numberOfLines={_numberOfLines}
-          style={textStyles}>
-          {children}
+    <View>
+      <Text
+        onTextLayout={onTextLayout}
+        numberOfLines={textshown ? undefined : numberOfLines}>
+        {children}
+      </Text>
+      {lengthMore ? (
+        <Text onPress={toggleNumberOfLines} style={styles.showMore}>
+          {textshown ? 'Read less' : 'Read more'}
         </Text>
-        {renderFooter()}
-      </View>
-      {renderFullText()}
+      ) : null}
     </View>
   );
 };
@@ -136,5 +55,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
   },
-  viewMoreText: {},
+  showMore: {
+    color: '#969696',
+    marginTop: 5,
+  },
 });
